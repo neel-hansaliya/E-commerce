b2432e94566893197e431b67023a1f91a0199a62
NEEL HANSALIYA <hansaliyaneel827@gmail.com>
2025-03-22 09:44:52 +0530

    some changes

--- src/app/components/Hero.tsx
+++ src/app/components/Hero.tsx
@@ -1,17 +1,17 @@
 import Image from "next/image";
-import h1 from "../assets/Grey Minimalist Special Offer Banner Landscape.png"
+import h1 from "../assets/Grey Minimalist Special Offer Banner Landscape.png";
+
 export default function Hero() {
   return (
     <section className="relative bg-[#F8FAFC] text-black text-center">
-      <div className="relative w-full h-[550px] overflow-hidden">
-        <div className="w-full h-full flex">
-          <Image
-            src={h1}
-            alt="Product 1"
-            width={800}
-            className="w-full h-full"
-          />
-        </div>
+      <div className="relative w-full h-[300px] sm:h-[400px] md:h-[500px] lg:h-[550px] overflow-hidden">
+        <Image
+          src={h1}
+          alt="Product 1"
+          layout="fill"
+          objectFit="cover"
+          className="w-full h-full"
+        />
       </div>
     </section>
   );
